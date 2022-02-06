367e5ce7548a8a8df7b0e604a96d2d5faa161b01
Nastya <n_m_666@mail.ru>
2022-02-06 22:36:38 +0500

    added smartBanner

---
+++ smartBanner_mr/src/index.js
@@ -0,0 +1,10 @@
+
+
+import "./style.scss"
+import "./scripts/scroll"
+import "./scripts/consultant"
+import "./scripts/reproduction"
+import "./scripts/wow"
+import "./scripts/data"
+import './styles/an.css';
+import "./scripts/modal"
