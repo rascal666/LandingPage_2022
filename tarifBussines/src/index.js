7bbd53679c59e1c2cd1f198ae428e92cbf2dbafe
Nastya <n_m_666@mail.ru>
2022-02-08 20:58:27 +0500

    addit copirat smartBunner

---
+++ tarifBussines/src/index.js
@@ -0,0 +1,9 @@
+import "./style.scss"
+import "./scripts/scroll"
+import "./scripts/consultant"
+import "./scripts/reproduction"
+import './styles/an.css';
+import "./scripts/data"
+import "./scripts/setting-swiper"
+
+// import "./styles/an.css"
---
+++ tarifBussines/src/scripts/data.js
@@ -0,0 +1,23 @@
+const dom = document.querySelector('.js-body')
+import anime from 'animejs/lib/anime.es.js'
+import WOW from './wow'
+
+
+let currentData = new Date();
+const month = currentData.getMonth() + 1
+let data = currentData.getDate() + '' + month + '22'
+
+new WOW().init();
+
+anime({
+    targets: '.rascal',
+    translateX: 270,
+    direction: 'alternate',
+    loop: true,
+    easing: 'easeInOutQuad'
+});
+
+
+
+dom.innerHTML = dom.innerHTML.replace(/curData/gi, data)
+
